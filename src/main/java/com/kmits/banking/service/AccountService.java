81ddd0c1897690f96b8b6f1869aaf9bd5ba90e7f
idrisseaa <idrisseaa93@gmail.com>
2024-11-20 23:25:38 +0100

    Added get all acounts REST API

--- src/main/java/com/kmits/banking/service/AccountService.java
+++ src/main/java/com/kmits/banking/service/AccountService.java
@@ -2,6 +2,8 @@ package com.kmits.banking.service;
 
 import com.kmits.banking.dto.AccountDto;
 
+import java.util.List;
+
 public interface AccountService {
 
     AccountDto createAccount(AccountDto accountDto);
@@ -11,5 +13,7 @@ public interface AccountService {
     AccountDto deposit(Long id, double amount);
 
     AccountDto withdraw(Long id, double amount);
+
+    List<AccountDto> getAllAccounts();
 
 }
--- src/main/java/com/kmits/banking/service/impl/AccountServiceImpl.java
+++ src/main/java/com/kmits/banking/service/impl/AccountServiceImpl.java
@@ -7,6 +7,10 @@ import com.kmits.banking.repository.AccountRepository;
 import com.kmits.banking.service.AccountService;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collector;
+import java.util.stream.Collectors;
+
 @Service
 public class AccountServiceImpl implements AccountService {
 
@@ -53,5 +57,12 @@ public class AccountServiceImpl implements AccountService {
         account.setBalance(total);
         Account savedAccount = accountRepository.save(account);
         return AccountMapper.mapToAccountDto(savedAccount);
+    }
+
+    @Override
+    public List<AccountDto> getAllAccounts() {
+        List <Account> accounts = accountRepository.findAll();
+        return  accounts.stream().map((account) -> AccountMapper.mapToAccountDto(account))
+                .collect(Collectors.toList());
     }
 }
